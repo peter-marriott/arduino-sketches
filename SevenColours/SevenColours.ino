75c1f3e5c82d7bd3920370f8652e32c29f722d21
Peter Marriott <psm@catalystcomputing.co.uk>
2014-05-06 22:37:57 +0100

    Basic LED sketch

---
+++ SevenColours/SevenColours.ino
@@ -0,0 +1,37 @@
+/*
+  Cycle through seven colours that can be displayed by a RGB led only using
+  Outputs as on or off not PWM (Pulse-width modulation)
+ */
+ 
+int red = 13;
+int green = 12;
+int blue = 11;
+
+boolean colourPatern[8][3] = { { false, false, false },
+{ false, false,  true },
+{ false,  true, false },
+{ false,  true,  true },
+{  true, false, false },
+{  true, false,  true },
+{  true,  true, false },
+{  true,  true,  true } };
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(red, OUTPUT);     
+  pinMode(green, OUTPUT);     
+  pinMode(blue, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+  for (byte i = 0;i < 8; i++) {
+    digitalWrite(red,   colourPatern[i][0]);
+    digitalWrite(green, colourPatern[i][1]);
+    digitalWrite(blue,  colourPatern[i][2]);
+    delay(1000);
+  }
+  
+}
